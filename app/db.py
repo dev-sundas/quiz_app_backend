f9118d58c39e247ef4bb85c5d4687caa4390ac2d
dev-sundas <hassan922837@gmail.com>
2025-09-16 22:09:12 +0500

    remove ssl from db.py

--- app/db.py
+++ app/db.py
@@ -3,16 +3,10 @@ from sqlmodel.ext.asyncio.session import AsyncSession
 from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
 from app.config import settings
 from typing import AsyncGenerator
-import ssl
 
 db_url_str = settings.get_db_url()
 
-# Create an SSL context
-ssl_context = ssl.create_default_context(cafile=None)
-ssl_context.check_hostname = False
-ssl_context.verify_mode = ssl.CERT_NONE
-
-engine = create_async_engine(db_url_str, echo=True, future=True,  connect_args={"ssl": ssl_context})
+engine = create_async_engine(db_url_str, echo=True, future=True)
 AsyncSessionLocal = async_sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
 
 async def create_db_and_tables():
